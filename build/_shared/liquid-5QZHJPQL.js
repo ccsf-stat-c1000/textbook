3ef93b977b74356caa5c17eafc8c010390e6552e
shawnwiggins <shawnwiggins@users.noreply.github.com>
2026-05-08 20:24:22 +0000

    deploy: ccsf-stat-c1000/textbook-dev@215b14b1a2a7968cfa9600581d1a9c16f67a2210

--- build/_shared/liquid-5QZHJPQL.js
+++ build/_shared/liquid-5QZHJPQL.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-5S6BNNID.js";import"/build/_shared/chunk-J3PKFMLQ.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/textbook/build/_shared/chunk-5S6BNNID.js";import"/textbook/build/_shared/chunk-J3PKFMLQ.js";import"/textbook/build/_shared/chunk-OZE3FFNP.js";export default a();
